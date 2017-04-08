9bb31e624212ec85f03ea385db4b88133a0ef1f5
Paul Antonio <paul.antonio2017@gmail.com>
2017-04-08 10:37:00 -0500

    Initial Push

---
+++ YahtzeeWithATwist/Classes/Sample.cs
@@ -0,0 +1,109 @@
+﻿// Programmer:  Paul Antonio
+// Date:        April 27, 2017
+// File Name:   Sample.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeWithATwist.Classes
+{
+    class Sample
+    {
+        /*************************************************************/
+        /*                       Documentation                       */
+        /*************************************************************/
+        #region Documentation
+        // Programmer: 
+
+        #region Overview
+        // --------------------
+        /// <summary>
+        /// </summary>
+        #endregion
+
+        #region Exceptions Thrown
+        // --------------------
+        #endregion
+
+        #region Development
+        // --------------------
+        // TODO:
+        #endregion
+        #endregion
+
+        /*************************************************************/
+        /*                           Data                            */
+        /*************************************************************/
+        #region Data Elements
+        #region Fields
+        // --------------------
+        #endregion
+
+        #region Properties
+        // --------------------
+        #endregion
+
+        #region Structures
+        // --------------------
+        #endregion
+
+        #region Objects
+        // --------------------
+        #endregion
+
+        #region Collections
+        // --------------------
+        #endregion
+
+        #region Delegates
+        // --------------------
+        #endregion
+        #endregion
+
+        /*************************************************************/
+        /*                       Functionality                       */
+        /*************************************************************/
+        #region Methods
+        #region Constructors
+        // --------------------
+        #endregion
+
+        #region Accessors
+        // --------------------
+
+        /// <summary>
+        ///     Description displayed in intelliSense
+        /// </summary>
+        /// 
+        /// <param name="">
+        ///     Description of parameter
+        /// </param>
+        /// 
+        /// <returns>
+        ///     Description of return value
+        /// </returns>
+        public static int method(int something)
+        {
+            #region Data
+            #endregion
+
+            #region Logic
+            #endregion
+
+            // TODO: return;
+        }
+        #endregion
+
+        #region Mutators
+        // --------------------
+        #endregion
+
+        #region Other Methods
+        // --------------------
+        #endregion
+        #endregion
+    }
+}
---
+++ YahtzeeWithATwist/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿// Programmers:
+// - Paul Antonio
+// - Seth Neds
+//
+// Date:        April 27, 2017
+// File Name:   MainPage.xaml.cs
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using YahtzeeWithATwist.Classes;
+
+namespace YahtzeeWithATwist
+{
+    /// <summary>
+    ///     Manages the front-end main page of the Yahtzee Game
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
